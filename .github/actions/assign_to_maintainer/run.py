bba9045a82bfdfe89845dd9137d64269919ef4b0
abikouo <abikouo@redhat.com>
2023-04-04 15:00:05 +0200

    full script

--- .github/actions/assign_to_maintainer/run.py
+++ .github/actions/assign_to_maintainer/run.py
@@ -2,11 +2,26 @@
 
 import os
 import logging
+import requests
 
 FORMAT = "[%(asctime)s] - %(message)s"
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger("assign_to_maintainer")
 logger.setLevel(logging.DEBUG)
+
+
+def add_assignee(url, token, assignee):
+    """
+        curl -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}"
+          -X POST
+          -d '{"assignees": [assignee]}'
+          https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/assignees
+    """
+    assignee = dict(assignees=[assignee])
+    headers = dict(Authorization="token %s" % token)
+
+    response = requests.post(url, json=assignee, headers=headers)
+    logger.info("JSON Response => %s", response.json())
 
 
 def main():
@@ -17,7 +32,8 @@ def main():
 
     url = f"https://api.github.com/repos/{pr_repository}/issues/{pr_number}/assignees"
 
-    logger.info("POST to URL => '%s'", url)
+    logger.info("URL => '%s'", url)
+    add_assignee(url, token, "abikouo")
 
 if __name__ == "__main__":
     main()
